74f4333aefae38c4cd9933f255052858bda31f6d
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-11-02 00:54:11 -0700

    adds code for providing an actual express server

--- src/expressProvider.ts
+++ src/expressProvider.ts
@@ -2,10 +2,11 @@ import {ProvidesExpressApplication} from './interfaces/providesExpressApplicatio
 import {Application} from 'express-serve-static-core';
 import {injectable} from 'inversify';
 import 'reflect-metadata';
+import * as express from 'express';
 
 @injectable()
 export class ExpressProvider implements ProvidesExpressApplication {
     getApp(): Application {
-        return undefined;
+        return express();
     }
 }
